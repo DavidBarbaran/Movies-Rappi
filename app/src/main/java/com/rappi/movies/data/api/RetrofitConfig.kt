de3993876fee0ea415d7ef71c9790e8d9517e15e
davidbarbaran <alejandro.david.barbaran@gmail.com>
2021-10-26 09:43:59 -0500

    Impl dimens & retrofit cache

--- app/src/main/java/com/rappi/movies/data/api/RetrofitConfig.kt
+++ app/src/main/java/com/rappi/movies/data/api/RetrofitConfig.kt
@@ -1,24 +1,31 @@
 package com.rappi.movies.data.api
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import com.rappi.movies.BuildConfig
+import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
-object RetrofitConfig {
+class RetrofitConfig(private val context: Context) {
 
-    private const val TIMEOUT_READ = 40L
-    private const val TIMEOUT_WRITE = 40L
-    private const val TIMEOUT_CONNECT = 30L
+    private val cacheSize = (5 * 1024 * 1024).toLong()
+
+    private val myCache = Cache(context.cacheDir, cacheSize)
 
     val okHttpClient: OkHttpClient = OkHttpClient
         .Builder()
+        .cache(myCache)
         .readTimeout(TIMEOUT_READ, TimeUnit.SECONDS)
         .writeTimeout(TIMEOUT_WRITE, TimeUnit.SECONDS)
         .addInterceptor(getLoggerInterceptor())
         .addInterceptor(getAddQueryInterceptor())
+        .addInterceptor(getCacheInterceptor())
         .connectTimeout(TIMEOUT_CONNECT, TimeUnit.SECONDS)
         .build()
 
@@ -41,5 +48,45 @@ object RetrofitConfig {
                 return chain.proceed(request)
             }
         }
+    }
+
+    private fun getCacheInterceptor(): Interceptor {
+        return object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                var request = chain.request()
+                request = if (isNetworkAvailable(context))
+                    request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+                else request.newBuilder().header(
+                    "Cache-Control",
+                    "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7
+                ).build()
+                return chain.proceed(request)
+            }
+        }
+    }
+
+    private fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        } else {
+            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+            return nwInfo.isConnected
+        }
+    }
+
+    companion object {
+        private const val TIMEOUT_READ = 40L
+        private const val TIMEOUT_WRITE = 40L
+        private const val TIMEOUT_CONNECT = 30L
     }
 }
--- app/src/main/java/com/rappi/movies/data/di/DataModule.kt
+++ app/src/main/java/com/rappi/movies/data/di/DataModule.kt
@@ -6,6 +6,7 @@ import com.rappi.movies.data.api.RestApi
 import com.rappi.movies.data.api.RetrofitConfig
 import com.rappi.movies.data.movie.MovieMapper
 import com.rappi.movies.data.movie.video.VideoMapper
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,7 +16,7 @@ val dataModule = module {
     single {
         Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
-            .client(RetrofitConfig.okHttpClient)
+            .client(get())
             .addConverterFactory(
                 GsonConverterFactory.create(
                     GsonBuilder().serializeNulls().create()
@@ -23,6 +24,8 @@ val dataModule = module {
             )
             .build().create(RestApi::class.java)
     }
+
+    factory { RetrofitConfig(androidContext()).okHttpClient }
 
     factory { MovieMapper() }
     factory { VideoMapper() }
